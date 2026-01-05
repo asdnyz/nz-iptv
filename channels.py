2d45304944ce03bc8353f1130129cdad0d061af6
asad <57964372+asdnyz@users.noreply.github.com>
2026-01-05 23:35:00 +0530

    Create channels.py

---
+++ channels.py
@@ -0,0 +1,11 @@
+# channels.py
+
+# Format: "Channel Name": ["M3U8_URL", "Category"]
+CHANNELS = {
+    "India Today": ["https://indiatodaylive-lh.akamaihd.net/i/itw_en@523030/index_1200_av-p.m3u8", "News"],
+    "Republic World": ["https://republic-hls.akamaized.net/hls/live/2022416/republic/playlist.m3u8", "News"],
+    "DW News (English)": ["https://dwstream72-lh.akamaihd.net/i/dwstream72_1@123556/master.m3u8", "Global"],
+    "Al Jazeera": ["https://live-alsat7.akamaized.net/hls/live/2014235/alsat7/playlist.m3u8", "Global"],
+    "NASA TV": ["https://ntv1.akamaized.net/hls/live/2014049/NASA-NTV1-HLS/master.m3u8", "Science"],
+    # You can easily add hundreds more here later
+}
