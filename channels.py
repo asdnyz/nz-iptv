17543b4e72ddfb6266595f4d0e0d0e57834a346b
asad <57964372+asdnyz@users.noreply.github.com>
2026-01-06 00:04:41 +0530

    Update channels.py

--- channels.py
+++ channels.py
@@ -1,8 +1,8 @@
 # channels.py
 CHANNELS = {
-    "1. TEST: Bunny (Always Works)": ["https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8", "Test"],
-    "2. Al Jazeera (English)": ["https://live-hls-web-aje.getaj.net/AJE/index.m3u8", "Global"],
-    "3. DW News": ["https://dwstream72-lh.akamaihd.net/i/dwstream72_1@123556/master.m3u8", "Global"],
-    "4. NASA TV": ["https://ntv1.akamaized.net/hls/live/2014049/NASA-NTV1-HLS/master.m3u8", "Science"],
-    "5. France 24 (English)": ["https://static.france24.com/live/F24_EN_LO_HLS/live_web.m3u8", "Global"]
+    "0. TEST: Apple (Always Works)": ["https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8", "Test"],
+    "1. Sky News (Public)": ["https://skynewsau-live.akamaized.net/hls/live/2002689/skynewsau-extra1/master.m3u8", "Global"],
+    "2. Al Jazeera English": ["https://live-hls-web-aje.getaj.net/AJE/index.m3u8", "Global"],
+    "3. France 24": ["https://static.france24.com/live/F24_EN_LO_HLS/live_web.m3u8", "Global"],
+    "4. NASA TV": ["https://ntv1.akamaized.net/hls/live/2014049/NASA-NTV1-HLS/master.m3u8", "Science"]
 }
