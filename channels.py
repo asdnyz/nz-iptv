726f08cc09c365dd38db0cd413b8c860719fc72b
asad <57964372+asdnyz@users.noreply.github.com>
2026-01-05 23:41:38 +0530

    Update channels.py

--- channels.py
+++ channels.py
@@ -2,10 +2,11 @@
 
 # Format: "Channel Name": ["M3U8_URL", "Category"]
 CHANNELS = {
-    "India Today": ["https://indiatodaylive-lh.akamaihd.net/i/itw_en@523030/index_1200_av-p.m3u8", "News"],
-    "Republic World": ["https://republic-hls.akamaized.net/hls/live/2022416/republic/playlist.m3u8", "News"],
-    "DW News (English)": ["https://dwstream72-lh.akamaihd.net/i/dwstream72_1@123556/master.m3u8", "Global"],
-    "Al Jazeera": ["https://live-alsat7.akamaized.net/hls/live/2014235/alsat7/playlist.m3u8", "Global"],
+    "DEBUG: Test Stream": ["https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8", "Test"],
+    "Aaj Tak HD": ["https://aajtakhdlive-amd.akamaized.net/hls/live/2014415/aajtakhd/aajtakhdlive/live_720p/chunks.m3u8", "News"],
+    "Al Jazeera English": ["https://live-alsat7.akamaized.net/hls/live/2014235/alsat7/playlist.m3u8", "Global"],
+    "DW News": ["https://dwstream72-lh.akamaihd.net/i/dwstream72_1@123556/master.m3u8", "Global"],
     "NASA TV": ["https://ntv1.akamaized.net/hls/live/2014049/NASA-NTV1-HLS/master.m3u8", "Science"],
-    # You can easily add hundreds more here later
+    "NDTV 24x7": ["https://ndtv.pc.cdn.bitgravity.com/ndtv/live/native/247.m3u8", "News"],
+    "DD National": ["https://prasarbharati.pc.cdn.bitgravity.com/ddnational/live/native/ddnational.m3u8", "News"]
 }
